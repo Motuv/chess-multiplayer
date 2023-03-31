a59ff47730ad810034cb31cc5e3be8492da8ed38
Motuv <piotrmiroslaw1@gmail.com>
2023-03-31 19:28:27 +0200

    added moves notation

--- src/components/MovesTable.js
+++ src/components/MovesTable.js
@@ -1,13 +1,13 @@
 import React from 'react';
-
+import Parser from 'html-react-parser';
 
 const MovesTable = (props) => {
-
+    const record = props.record.pgn({maxWidth: 5, newline: '<br>'})
     return(
         <div className="leftSideBar">
             <h2>Moves</h2>
             <div className='moves'>
-                {props.record.pgn({maxWidth: 5, newline: '</br>'}).replace('\'','')}
+                {Parser(record)}
             </div>
         </div>);
 }
